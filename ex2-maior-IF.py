7f2c3dd056e945974c865be28b46c58d5cc7cf34
ViniciusVianna999 <viniciussilvavianna@outlook.com>
2026-09-10 20:06:02 -0300

    Add files via upload

---
+++ ex2-maior-IF.py
@@ -0,0 +1,8 @@
+
+
+if numero1 > numero2 and numero1 > numero3:
+   print('O primeiro número é o maior')
+elif numero2 > numero3:
+   print('O segundo número é o maior')
+else:
+   print('O terceiro número é o maior')
