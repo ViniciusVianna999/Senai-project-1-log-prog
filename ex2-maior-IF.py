ccd840ae73d48a9bcf6de4e2795f2a612fc55a1a
ViniciusVianna999 <viniciussilvavianna@outlook.com>
2026-09-10 20:14:12 -0300

    Add files via upload

--- ex2-maior-IF.py
+++ ex2-maior-IF.py
@@ -1,5 +1,6 @@
 
 
+
 if numero1 > numero2 and numero1 > numero3:
    print('O primeiro número é o maior')
 elif numero2 > numero3:
---
+++ primeiro.py
@@ -0,0 +1,17 @@
+# criar variavel
+# dev de sistemas
+
+# snake_case -> caso cobra (underline no lugar do espaço)
+dev_de_sistemas = ''
+
+# camelCase -> caso camelo (Letra maiuscula no inicio da
+# segunda palavra)
+devDeSistema = ''
+
+# PascalCase -> Todas as palavras com letra maiuscula
+# usado em Classes
+DevDeSistemas = object()
+
+# pedir um numero ao usuario
+numero = input('Digite um numero: ')
+print(type(numero))
