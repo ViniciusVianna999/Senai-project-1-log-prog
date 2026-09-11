4f74d7c06782b72e1895acee9d5a2fb5d4415d74
ViniciusVianna999 <viniciussilvavianna@outlook.com>
2026-09-10 21:26:18 -0300

    Add files via upload

---
+++ ex3-jokenpo-IF.py
@@ -0,0 +1,32 @@
+# Construa um jogo de pedra, papel e tesoura.
+
+jogador1 = input('Pedra, Papel ou Tesoura: ')
+jogador2 = input('Pedra, Papel ou Tesoura: ')
+
+# primeiro bloco -> jogador1 == Pedra
+if (jogador1 == 'Pedra'):
+    if(jogador2 == 'Pedra'):
+        print('Empate')
+    if(jogador2 == 'Papel'):
+        print('Jogador 2 Ganhou!')
+    if(jogador2 == 'Tesoura'):
+        print('Jogador 1 Ganhou')
+
+# segundo bloco -> jogador1 == Papel
+if jogador1 == 'Papel':
+    if(jogador2 == 'Pedra'):
+        print('Jogador 1 ganhou')
+    if(jogador2 == 'Papel'):
+        print('empate')
+    if(jogador2 == 'Tesoura'):
+        print('Jogador 2 Ganhou')
+
+# terceiro bloco -> jogador 1 == tesoura
+if jogador1 == 'Tesoura':
+    if(jogador2 == 'Pedra'):
+        print('Jogador 2 Ganhou!')
+    if(jogador2 == 'Papel'):
+        print('Jogador 1 Ganhou!')
+    if(jogador2 == 'Tesoura'):
+        print('Empate')
+    
